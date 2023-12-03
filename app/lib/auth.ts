4d662a1ffea480f02d5ea565afe9417809466c4b
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-12-02 23:51:26 -0600

    Connect to PG database

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,20 @@
 import GoogleProvider from "next-auth/providers/google";
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "pg";
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
+const POOL = new  Pool({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  })
 
 const AuthOptions = {
+    adapter: PostgresAdapter(POOL),
     providers: [
         GoogleProvider({
             clientId: GOOGLE_CLIENT_ID,
